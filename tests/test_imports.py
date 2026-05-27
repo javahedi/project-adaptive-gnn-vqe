97bb61deb6717b5d0153d5811dda751db6aca1af
javahedi <javahedi@gmail.com>
2026-05-27 15:35:18 +0200

    Add reusable XXZ dataset builder

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,9 +1,10 @@
 from adaptive_gnn_vqe.models import PointerGNN
 from adaptive_gnn_vqe.vqe import run_one_realization
 from adaptive_gnn_vqe.training.losses import pointer_soft_loss
-
+from adaptive_gnn_vqe.data.builders import build_xxz_dataset
 
 def test_imports():
     assert PointerGNN is not None
     assert run_one_realization is not None
     assert pointer_soft_loss is not None
+    assert build_xxz_dataset is not None
